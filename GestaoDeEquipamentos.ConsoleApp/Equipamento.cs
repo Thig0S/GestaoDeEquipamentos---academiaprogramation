30a1de96a9c7f17ec972c611748d23587f890762
thig0S_ <thiagokovalski5@gmail.com>
2026-04-10 17:22:59 -0300

    add edicao de equipamento

--- GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -3,10 +3,14 @@ namespace GestaoDeEquipamentos.ConsoleApp;
 public class Equipamento
 {
     public string? Id;
-    public string? nome;
+    public string? Nome;
     public string? Fabricante;
     public decimal PrecoAquisicao;
     public DateTime DataFabricacao;
 
 
+    public Equipamento()
+    {
+
+    }
 }
--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using GestaoDeEquipamentos.ConsoleApp;
+﻿using System.Security.Cryptography;
+using GestaoDeEquipamentos.ConsoleApp;
 
 internal class Program
 {
@@ -8,7 +9,7 @@ internal class Program
 
         while (true)
         {
-            Console.ReadLine();
+            Console.Clear();
             Console.WriteLine("---------------------------------");
             Console.WriteLine("Gestão de Equipamentos");
             Console.WriteLine("---------------------------------");
@@ -24,7 +25,7 @@ internal class Program
             if (opcaoMenu == "S")
             {
 
-                Console.ReadLine();
+                Console.Clear();
                 break;
             }
 
@@ -40,9 +41,9 @@ internal class Program
                 do
                 {
                     System.Console.Write("\nDigite o NOME do equipamento: ");
-                    novoEquipamento.nome = Console.ReadLine();
+                    novoEquipamento.Nome = Console.ReadLine();
 
-                    if (!String.IsNullOrEmpty(novoEquipamento.nome) && novoEquipamento.nome.Length > 3)
+                    if (!String.IsNullOrEmpty(novoEquipamento.Nome) && novoEquipamento.Nome.Length > 3)
                     {
                         break;
                     }
@@ -71,6 +72,8 @@ internal class Program
                 System.Console.Write("\nDigite a DATA CADASTRADA do equipamento DD/MM/YYYY: ");
                 DateTime dataEquipamento = Convert.ToDateTime(Console.ReadLine());
 
+                novoEquipamento.Id = Convert.ToHexString(RandomNumberGenerator.GetBytes(20)).ToLower().Substring(0, 7);
+
                 for (int i = 0; i < equipamentos.Length; i++)
                 {
                     Equipamento? e = equipamentos[i];
@@ -82,7 +85,7 @@ internal class Program
                     }
                 }
                 Console.WriteLine("---------------------------------");
-                Console.WriteLine($"O equipamento \"{novoEquipamento.nome}\" foi cadastrado com sucesso!");
+                Console.WriteLine($"O equipamento \"{novoEquipamento.Nome}\" foi cadastrado com sucesso!");
                 Console.WriteLine("---------------------------------");
                 Console.WriteLine("Pressione ENTER para continuar...");
                 Console.ReadLine();
@@ -90,6 +93,107 @@ internal class Program
 
             else if (opcaoMenu == "2")
             {
+                System.Console.WriteLine("-------------------------------");
+                System.Console.WriteLine("Gestão de Equipamentos!");
+                System.Console.WriteLine("-------------------------------");
+                System.Console.WriteLine("Edição de Equipamento: ");
+
+                System.Console.WriteLine(
+                    "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}"
+                    , "ID", "Nome", "Fabricante", "Preço de Aquisição", "Data de Fabricação");
+
+                for (int i = 0; i < equipamentos.Length; i++)
+                {
+                    Equipamento e = equipamentos[i];
+
+                    if (e == null)
+                        continue;
+
+                    System.Console.WriteLine(
+                        "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}"
+                        , e.Id, e.Nome, e.Fabricante, e.PrecoAquisicao.ToString("C2"), e.DataFabricacao.ToShortDateString()
+                    );
+                }
+
+                string? idSelecionado;
+                do
+                {
+                    System.Console.Write("Digite o ID do produto que deseja EDITAR: ");
+                    idSelecionado = Console.ReadLine();
+
+                    if (!String.IsNullOrEmpty(idSelecionado) && idSelecionado.Length == 7)
+                        break;
+                } while (true);
+
+                Equipamento? equipamentoSelecionado = null;
+
+                for (int i = 0; i < equipamentos.Length; i++)
+                {
+                    Equipamento e = equipamentos[i];
+
+                    if (e == null)
+                        continue;
+
+                    if (e.Id == idSelecionado)
+                    {
+                        equipamentoSelecionado = e;
+                        break;
+                    }
+                }
+                if (equipamentoSelecionado == null)
+                {
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine($"O equipamento com o ID NÃO foi encontrado!");
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("Pressione ENTER para continuar...");
+                    Console.ReadLine();
+                    continue;
+                }
+                Equipamento novoEquipamento = new Equipamento();
+                do
+                {
+                    System.Console.Write("\nDigite o NOME do equipamento: ");
+                    novoEquipamento.Nome = Console.ReadLine();
+
+                    if (!String.IsNullOrEmpty(novoEquipamento.Nome) && novoEquipamento.Nome.Length > 3)
+                    {
+                        break;
+                    }
+                    System.Console.WriteLine("Nome deve conter no mínimo 3 caracteres!");
+                    Console.ReadLine();
+
+                } while (true);
+
+                do
+                {
+                    System.Console.Write("\nDigite o FABRICANTE do equipamento: ");
+                    novoEquipamento.Fabricante = Console.ReadLine();
+
+                    if (!String.IsNullOrEmpty(novoEquipamento.Fabricante) && novoEquipamento.Fabricante.Length > 3)
+                    {
+                        break;
+                    }
+                    System.Console.WriteLine("Fabricante deve conter no mínimo 3 caracteres!");
+                    Console.ReadLine();
+
+                } while (true);
+
+                System.Console.Write("\nDigite o PREÇO do equipamento: ");
+                novoEquipamento.PrecoAquisicao = Convert.ToDecimal(Console.ReadLine());
+
+                System.Console.Write("\nDigite a DATA CADASTRADA do equipamento DD/MM/YYYY: ");
+                DateTime dataEquipamento = Convert.ToDateTime(Console.ReadLine());
+
+                equipamentoSelecionado.Nome = novoEquipamento.Nome;
+                equipamentoSelecionado.Fabricante = novoEquipamento.Fabricante;
+                equipamentoSelecionado.PrecoAquisicao = novoEquipamento.PrecoAquisicao;
+                equipamentoSelecionado.DataFabricacao = novoEquipamento.DataFabricacao;
+
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine($"O equipamento \"{equipamentoSelecionado.Id}\" foi EDITADO com sucesso!");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Pressione ENTER para continuar...");
+                Console.ReadLine();
 
             }
 
